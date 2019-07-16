1fb2353adf0df6c2784c18a872b35bfb43424bcf
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2019-07-15 22:13:42 -0500

    simple tag test

--- library/aws_tagging.py
+++ library/aws_tagging.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from ansible.module_utils.basic import *
+from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info
+import requests, json
+import sys
 import boto3
 import botocore
 
-def main():
+def tag_ec2():
 
     resource = "i-03ba8b4dde20e9a66"
 
@@ -21,6 +25,34 @@ def main():
         },
     ] 
     )
+
+
+def tagging(params):
+    role = False
+    resource = params["resource"]
+    tags = params["tags"]
+    region = params["region"]
+
+    return False, True, json.dumps(tags)
+
+def main():
+
+    fields = {
+        "role": {"required": False, "type": "str"},
+        "resource": {"required": True, "type": "str"},
+        "tags": {"required": True, "type": "str"},
+        "region": {"required": False, "type": "str"}
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+
+    is_error, has_changed, result = tagging(module.params)
+
+    if not is_error:
+        module.exit_json(changed=has_changed, msg=result)
+    else:
+        module.fail_json(msg=result)
+
 
 if __name__ == "__main__":
     main()
